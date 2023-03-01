8e3ac0c71af67648cc91af60f76e12d0b2221146
romain2518 <rolenoir25@gmail.com>
2023-03-01 13:20:16 +0100

    Fix new message auto resize

--- docs/model/script.js
+++ docs/model/script.js
@@ -87,13 +87,13 @@ const handleUserCardClick = function (event) {
 
     event.currentTarget.classList.remove('has-notification');
     event.currentTarget.classList.add('active');
+
+    document.querySelector('section:last-child').scrollIntoView({ behavior: "smooth", block: "nearest" });
+    document.querySelector('section:last-child').focus();
 }
 
-const handleNewMessageTextareaInput = function (event) {
-    // Resize textarea
-    event.currentTarget.style.height = `${event.currentTarget.scrollHeight}px`;
-
-    // Resize chat box based on textarea height
+const resizeMessageList = function (event) {
+    // Resize message liste based on textarea height
     const
         himselfHeight = document.querySelector('.himself').offsetHeight,
         himselfMarginBottom = parseInt(
@@ -105,17 +105,68 @@ const handleNewMessageTextareaInput = function (event) {
     document.querySelector('.chat').style.height = `calc(100% - ${himselfHeight + himselfMarginBottom}px - ${newMessageHeight}px)`;
 }
 
+function getCaretCharacterOffsetWithin(element) {
+    let caretOffset = 0;
+    const doc = element.ownerDocument || element.document;
+    const win = doc.defaultView || doc.parentWindow;
+    let sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            const range = win.getSelection().getRangeAt(0);
+            const preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+        }
+    } else if ((sel = doc.selection) && sel.type != "Control") {
+        const textRange = sel.createRange();
+        const preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}  
+
+const handleTextareaBeforeInput = function (event) {
+    if (!event.inputType.match(/^insert(?!Text)[A-Za-z]+$/)) return; // If event type is an insert but not insertText (paste, drop etc.)
+    event.preventDefault();
+
+    // Add text in plain text
+    const caretPos = getCaretCharacterOffsetWithin(event.currentTarget);
+    const textToAdd = event.dataTransfer.getData('text/plain');
+
+    event.currentTarget.textContent = 
+        event.currentTarget.textContent.slice(0, caretPos)
+        + textToAdd
+        + event.currentTarget.textContent.slice(caretPos);
+
+    // Set back carret
+    const range = document.createRange();
+    const sel = window.getSelection();
+    
+    range.setStart(event.currentTarget.childNodes[0], caretPos + textToAdd.length);
+    range.collapse(true);
+    
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+    // Trigger input event to resize the message list
+    event.currentTarget.dispatchEvent(new Event('input'));
+}
+
 const handleTextareaFocus = function (event) {
     event.currentTarget.classList.add('active');
 }
 
 const handleTextareaBlur = function (event) {
-    if (event.currentTarget.value !== '') return;
+    if (event.currentTarget.textContent !== '') return;
     event.currentTarget.classList.remove('active');
 }
 
 const handleTextareaPlaceholderClick = function () {
-    document.querySelector('.new-message textarea').focus();
+    document.querySelector('.new-message .textarea').focus();
 }
 
 window.addEventListener('click', handleWindowClick);
@@ -136,9 +187,10 @@ document.querySelectorAll('.user-card').forEach(card => {
     card.addEventListener('click', handleUserCardClick);
 });
 
-document.querySelector('.new-message textarea').addEventListener('input', handleNewMessageTextareaInput);
-document.querySelector('.new-message textarea').dispatchEvent(new Event("input"));
-document.querySelector('.new-message textarea').addEventListener('focus', handleTextareaFocus);
-document.querySelector('.new-message textarea').addEventListener('blur', handleTextareaBlur);
+document.querySelector('.new-message .textarea').addEventListener('beforeinput', handleTextareaBeforeInput);
+document.querySelector('.new-message .textarea').addEventListener('input', resizeMessageList);
+document.querySelector('.new-message .textarea').dispatchEvent(new Event('input'));
+document.querySelector('.new-message .textarea').addEventListener('focus', handleTextareaFocus);
+document.querySelector('.new-message .textarea').addEventListener('blur', handleTextareaBlur);
 
 document.querySelector('.new-message .placeholder').addEventListener('click', handleTextareaPlaceholderClick);
