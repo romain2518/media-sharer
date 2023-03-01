642e440fc13d3482a8668b8d42717dd8f18ff1ae
romain2518 <rolenoir25@gmail.com>
2023-03-01 19:00:31 +0100

    Rename User::status to User::isRead

---
+++ migrations/Version20230301175850.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230301175850 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE status ADD is_read TINYINT(1) NOT NULL, DROP status');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE status ADD status VARCHAR(10) NOT NULL, DROP is_read');
+    }
+}
